1a8a97d29eac6cecb9a1cdb1cb5895b55458bcd9
Sambit Mishra <labsquantumleap@gmail.com>
2024-08-29 13:27:09 +0530

    Update Master-Program.py

--- Master-Program.py
+++ Master-Program.py
@@ -138,7 +138,7 @@ def main():
 
     if service == 'Equity Analysis':
         ticker = st.text_input("Enter Yahoo Finance Ticker Symbol", "AAPL")
-        period = st.selectbox("Select Data Period", ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y'])
+        period = st.selectbox("Select Data Period", ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', 'max'])
         interval = st.selectbox("Select Data Interval", ['1m', '2m', '5m', '15m', '30m', '60m', '90m', '1d', '5d', '1wk', '1mo'])
 
         if st.button("Analyze"):
